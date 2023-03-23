16360891cc7f1a7784afceaa817ef8673d29cdd9
Roman Kuzmich <rvku2mich@yandex.ru>
2023-03-23 10:56:39 +0300

    Program developed

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = ReadInt("Введите размер массива: ");
+string[] array = new string[n];
+for (int i = 0; i < n; i++)
+{
+    System.Console.WriteLine($"Введите {i}-й элемент массива: ");
+    array[i] = Console.ReadLine();
+}
+System.Console.WriteLine();
+System.Console.WriteLine();
+List<string> newArray = new List<string>();
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray.Add(array[i]);
+    }
+}
+System.Console.Write("[");
+foreach (string element in newArray)
+{
+    System.Console.Write(element + " ");
+}
+System.Console.Write("]");
